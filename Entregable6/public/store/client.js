04135b0dde341a746469116a155995d94b67da4a
Bruno Aguirre Peña <bruno.aguirre.p1997@gmail.com>
2022-07-01 20:05:51 -0500

    entregable6

--- Entregable6/public/store/client.js
+++ Entregable6/public/store/client.js
@@ -2,6 +2,15 @@ const socket = io();
 
 const productsDiv = document.getElementById('products');
 
+const form = document.getElementById('form-product');
+const title = document.getElementById('title');
+const price = document.getElementById('price');
+const thumbnail = document.getElementById('thumbnail');
+const successModal = document.querySelector('.exito');
+const errorModal = document.querySelector('.error');
+form.addEventListener('submit',sendProduct);
+
+
 socket.on('populateProducts', payload => {
     populateProducts(payload);
 });
@@ -18,4 +27,39 @@ const populateProducts = async(payload) => {
             productsDiv.innerHTML += filled;
         });
     }
+}
+
+async function sendProduct(event) {
+    try {
+        const titleData = title.value;
+        const priceData = price.value;
+        const thumbnailData = thumbnail.value;
+
+        if([titleData,priceData,thumbnailData].includes('')){
+            errorModal.classList.remove('hidden');
+            setTimeout(() => {
+                errorModal.classList.add('hidden');
+            }, 2000);
+        }else{
+            let data = {
+                title:titleData,
+                price:priceData,
+                thumbnail:thumbnailData
+            }
+
+            socket.emit('insertProduct',data);
+
+            title.value = '';
+            price.value = '';
+            thumbnail.value = '';
+
+            successModal.classList.remove('hidden');
+            setTimeout(() => {
+                successModal.classList.add('hidden')
+            }, 2000);
+        }
+    } catch(error) {
+        console.log(error);
+    }
+    event.preventDefault();
 }
